0196d02169186dc56de3464cefebc9494de2f116
Frank Condezo <frankcondezo@gmail.com>
2023-05-16 12:39:56 -0500

    setup boilerplate

---
+++ app/lib/utils.ts
@@ -0,0 +1,12 @@
+export enum Role {
+  USER = "USER",
+  ADMIN = "ADMIN",
+}
+
+export enum PostType {
+  FUNNY = "FUNNY",
+  TECH = "TECH",
+  RANDOM = "RANDOM",
+  NEWS = "NEWS",
+  SERIOUS = "SERIOUS",
+}
